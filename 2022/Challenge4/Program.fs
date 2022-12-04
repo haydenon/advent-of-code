d45604fb41c3ce46164864ae22657ddd21c24c36
Hayden O'Neill <hayden.oneill9@gmail.com>
2022-12-04 18:10:34 +1300

    Challenge 4

---
+++ 2022/Challenge4/Program.fs
@@ -0,0 +1,38 @@
+﻿open System
+
+let parseSections (sections : string) =
+  let parts = sections.Split("-")
+  (Int32.Parse parts[0], Int32.Parse parts[1])
+
+let parseRow (row : string) =
+  let elfAssignments = row.Split(",")
+  (parseSections elfAssignments[0], parseSections elfAssignments[1])
+
+let loadData () =
+    let text = System.IO.File.ReadAllLines("./input.txt")
+    text
+    |> List.ofArray
+    |> List.map parseRow
+
+let fullyContained ((amin, amax), (bmin, bmax)) =
+  (amin >= bmin && amax <= bmax) ||
+  (bmin >= amin && bmax <= amax)
+
+let overlap ((amin, amax), (bmin, bmax)) =
+  (amin >= bmin && amin <= bmax) ||
+  (bmin >= amin && bmin <= amax) ||
+  (amax >= bmin && amin <= bmax) ||
+  (bmax >= amin && bmax <= amax)
+
+let data = loadData ()
+
+data
+|> List.filter fullyContained
+|> List.length
+|> printfn "Part 1: %d"
+
+
+data
+|> List.filter overlap
+|> List.length
+|> printfn "Part 2: %d"
