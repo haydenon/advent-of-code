cf210d5445a7c198f38a50b86d93a84d234bc855
Hayden O'Neill <hayden.oneill9@gmail.com>
2022-12-13 18:55:08 +1300

    Challenge 13 part 1

---
+++ 2022/Challenge13/Program.fs
@@ -0,0 +1,86 @@
+﻿open System
+open System.Text.RegularExpressions
+
+type Packet =
+    | Num of int
+    | List of Packet list
+
+let parsePacket (str: string) =
+    let result =
+        Regex.Split(str, @"(\[|\]|\d+|,)")
+        |> Array.filter (String.IsNullOrWhiteSpace >> not)
+        |> Array.toList
+
+    let rec parseItems current (items: string list) =
+        match items with
+        | "[" :: rest ->
+            let children, rest = parseItems [] rest
+            parseItems (children :: current) rest
+        | "," :: rest -> parseItems current rest
+        | "]" :: rest -> (List(current |> List.rev), rest)
+        | num :: rest -> parseItems ((Int32.Parse num |> Num) :: current) rest
+        | [] -> List(current |> List.rev), []
+
+    match parseItems [] result with
+    | List (values), _ -> List.head values
+    | _ -> failwith ""
+
+let rec parsePacketPairs pairs (lines: string list) =
+    match lines with
+    | blank :: rest when blank |> String.IsNullOrWhiteSpace -> parsePacketPairs pairs rest
+    | first :: second :: rest ->
+        let pairs = (parsePacket first, parsePacket second) :: pairs
+        parsePacketPairs pairs rest
+    | _ -> pairs |> List.rev
+
+
+let loadData () =
+    let text = System.IO.File.ReadAllLines("./input.txt")
+
+    text |> Array.toList |> parsePacketPairs []
+
+let rec checkPair (item1, item2) =
+    match (item1, item2) with
+    | (Num a, Num b) ->
+        if a = b then None else Some(a < b)
+    | (List _, Num _) -> checkPair (item1, List([ item2 ]))
+    | (Num _, List _) -> checkPair (List([ item1 ]), item2)
+    | (List a, List b) ->
+        let aLength = List.length a
+        let bLength = List.length b
+
+        let aShorter = aLength < bLength
+        let bShorter = aLength > bLength
+
+        let a, b =
+            if aShorter then
+                (a, List.take aLength b)
+            else
+                (List.take bLength a, b)
+
+        let res =
+            List.zip a b
+            |> List.fold
+                (fun curr pair ->
+                    match curr with
+                    | None -> checkPair pair
+                    | curr -> curr)
+                None
+
+        printfn "%A %b" res aShorter
+
+        match res, aShorter with
+        | None, true -> Some true
+        | None, false -> if bShorter then Some false else None
+        | res, _ -> res
+
+
+let data = loadData ()
+
+data
+|> List.map (checkPair >> Option.get) //defaultValue false)
+|> List.indexed
+|> List.filter snd
+|> List.map (fst >> ((+)1))
+|> List.sum
+|> printfn "Part 1: %d"
