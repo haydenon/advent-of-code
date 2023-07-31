f669de622313c55b0b5ee419863e2e96279a4d32
Hayden O'Neill <hayden.oneill9@gmail.com>
2023-07-31 21:33:45 +1200

    Day 4 and part 1 of 5

---
+++ 2019/Challenge04/Program.fs
@@ -0,0 +1,48 @@
+﻿open System
+
+let loadData () =
+    let text = System.IO.File.ReadAllLines("./input.txt")
+    let parts = text[ 0 ].Split("-") |> Array.map Int32.Parse
+    (parts[0], parts[1])
+
+let (bottom, top) = loadData ()
+
+let matchesPasswordRule1 num =
+    let str = num.ToString()
+
+    let hasAdjacent (str: string) =
+        seq { 0 .. (str.Length - 2) }
+        |> Seq.exists (fun idx -> str[idx] = str[idx + 1])
+
+    let doesNotDecrease (str: string) =
+        seq { 0 .. (str.Length - 2) }
+        |> Seq.forall (fun idx -> str[idx] <= str[idx + 1])
+
+    hasAdjacent str && doesNotDecrease str
+
+let matchesPasswordRule2 num =
+    let str = num.ToString()
+    let length = str.Length
+
+    let hasAdjacent (str: string) =
+        seq { 0 .. (length - 2) }
+        |> Seq.exists (fun idx ->
+            str[idx] = str[idx + 1]
+            && (idx = length - 2 || str[idx + 2] <> str[idx])
+            && (idx = 0 || str[idx - 1] <> str[idx]))
+
+    let doesNotDecrease (str: string) =
+        seq { 0 .. (str.Length - 2) }
+        |> Seq.forall (fun idx -> str[idx] <= str[idx + 1])
+
+    hasAdjacent str && doesNotDecrease str
+
+seq { bottom..top }
+|> Seq.filter matchesPasswordRule1
+|> Seq.length
+|> printfn "Part 1: %d"
+
+seq { bottom..top }
+|> Seq.filter matchesPasswordRule2
+|> Seq.length
+|> printfn "Part 2: %d"
---
+++ 2019/Challenge05/Program.fs
@@ -0,0 +1,47 @@
+﻿open System
+
+let loadData () =
+    let text = System.IO.File.ReadAllLines("./input.txt")
+    text[ 0 ].Split(",") |> Array.map Int32.Parse
+
+let getOpCode num =
+    let opCode = num % 100
+    ((num / 10000 % 10 <> 0, num / 1000 % 10 <> 0, num / 100 % 10 <> 0), opCode)
+
+let rec runOpCode input pos (code: int []) =
+    let doMathOp func pos (last, mid, first) =
+        let l =
+            if first then
+                code[pos + 1]
+            else
+                code[code[pos + 1]]
+
+        let r =
+            if mid then
+                code[pos + 2]
+            else
+                code[code[pos + 2]]
+
+        let newPos = code[pos + 3]
+        code[newPos] <- func l r
+        runOpCode input (pos + 4) code
+
+    match getOpCode code[pos] with
+    | _, 99 -> Some code
+    | modes, 1 -> doMathOp (+) pos modes
+    | modes, 2 -> doMathOp (*) pos modes
+    | _, 3 ->
+        code[code[pos + 1]] <- input
+        runOpCode input (pos + 2) code
+    | _, 4 ->
+        code[code[pos + 1]] |> printfn "%d"
+        runOpCode input (pos + 2) code
+    | _ -> None
+
+let data = loadData ()
+
+data
+|> runOpCode 1 0
+// |> Option.get
+// |> fun arr -> Array.get arr 0
+|> printfn "Part 1: %A"
