2bde609d6303a257ac501b919a4468eda7a51f01
Hayden O'Neill <hayden.oneill9@gmail.com>
2024-12-12 21:28:29 +1300

    Part 2 progress

--- 2024/Challenge12/Program.fs
+++ 2024/Challenge12/Program.fs
@@ -12,6 +12,14 @@ let getAdjacent (x, y) =
     adjacent
     |> List.map (fun (dx, dy) -> (x + dx, y + dy))
 
+
+let diagonal = [ (1, 1); (1, -1); (-1, 1); (-1, -1) ]
+
+let getDiagonal (x, y) =
+    diagonal
+    |> List.map (fun (dx, dy) -> (x + dx, y + dy))
+
+
 let count (grid: char array array) =
     let width, height = grid[0].Length, grid.Length
 
@@ -21,17 +29,17 @@ let count (grid: char array array) =
         else
             grid[y][x] = grid[oy][ox]
 
-    let rec countBlock (visited: Set<int * int>, perimeter: int, area: int) coords =
+    let rec countBlock (visited: Set<int * int>, perimeter: ((int * int) * (int * int)) list, area: int) coords =
         if (visited |> Set.contains coords) then
             (visited, perimeter, area)
         else
             let adjacent = getAdjacent coords
 
-            let newPerimeter =
-                (adjacent
-                 |> List.filter (matches coords >> not)
-                 |> List.length)
-                + perimeter
+            let newPerimeterPieces =
+                (adjacent |> List.filter (matches coords >> not))
+                |> List.map (fun c -> (c, coords))
+
+            let newPerimeter = perimeter |> List.append newPerimeterPieces
 
             let newVisited = visited |> Set.add coords
             let newArea = area + 1
@@ -46,7 +54,7 @@ let count (grid: char array array) =
     allCoords
     |> Seq.fold
         (fun (vis, areas) coord ->
-            let input = (vis, 0, 0)
+            let input = (vis, [], 0)
             let res = countBlock input coord
 
             match res with
@@ -56,9 +64,68 @@ let count (grid: char array array) =
     |> snd
 
 
+let countSides (perimeter: ((int * int) * (int * int)) list) =
+    let unique = perimeter |> List.map fst |> Set.ofList
 
-data
-|> count
-|> List.map (fun (per, area) -> per * area)
+    let sideToAdjacent =
+        perimeter
+        |> List.fold
+            (fun acc (edge, inner) ->
+                acc
+                |> Map.change edge (function
+                    | None -> Some 1
+                    | Some count -> Some(count + 1)))
+            Map.empty
+
+    let visited = Set.empty
+
+    let rec count visited coords sides perimeter =
+        let adj = getAdjacent coords |> Set.ofList
+        let diagonal = getDiagonal coords |> Set.ofList
+        let reachable = perimeter |> Set.intersect (diagonal + adj)
+
+        if (reachable |> Set.isEmpty) then
+            sides
+        else
+            let newVisited = (visited |> Set.add coords)
+
+            let intersect = adj |> Set.intersect perimeter
+            let adjacent = intersect - newVisited
+
+
+            let onSameSide = adjacent |> Set.toList |> List.tryHead
+            // adjacent |> Set.toListperimeter |> S
+            // |> Set (fun other ->
+            //     not (visited |> Set.contains other)
+            //     && adjacent |> List.contains other)
+
+            match onSameSide with
+            | Some next -> count newVisited next sides (perimeter |> Set.remove coords)
+            | None ->
+                let corners = perimeter |> Set.intersect diagonal
+                // |> Set.filter (fun other ->
+                //     not (newVisited |> Set.contains other)
+                //     && diagonal |> List.contains other)
+
+                corners
+                |> Set.map (fun next ->
+                    count newVisited next (sides + (sideToAdjacent |> Map.find next)) (perimeter |> Set.remove coords))
+                |> Set.toList
+                |> List.max
+
+    let first = perimeter |> List.head |> fst
+    let res = count visited first 1 (unique |> Set.remove first)
+    printfn "%d" res
+    res
+
+let result = data |> count
+
+result
+|> List.map (fun (per, area) -> (per |> List.length) * area)
+|> List.sum
+|> printfn "Part 1: %d"
+
+result
+|> List.map (fun (per, area) -> (per |> countSides) * area)
 |> List.sum
 |> printfn "Part 1: %d"
